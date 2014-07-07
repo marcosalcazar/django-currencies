d2f082c5fb7f50fe9cb8629cfc8fb2b9c7b52a8d
Marcos Gabriel Alcázar <marcos.alcazar@gmail.com>
2014-07-07 15:48:13 -0300

    Added context var for active currencies only

--- currencies/context_processors.py
+++ currencies/context_processors.py
@@ -3,11 +3,12 @@ from currencies.models import Currency
 
 def currencies(request):
     currencies = Currency.objects.all()
-
+    currencies_active = Currency.objects.filter(is_active=True).all()
     if not request.session.get('currency_id'):
         request.session['currency_id'] = Currency.objects.get(is_default__exact=True).id
 
     return {
         'CURRENCIES': currencies,
+        'CURRENCIES_ACTIVE': currencies_active,
         'CURRENCY': Currency.objects.get(pk=request.session['currency_id'])
     }
